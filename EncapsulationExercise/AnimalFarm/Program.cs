8224baa00e622c843f5f3c3ddec056363302148a
dianGrigorov <diangrigorov@gmail.com>
2023-06-25 12:55:58 +0300

    .

---
+++ EncapsulationExercise/AnimalFarm/Program.cs
@@ -0,0 +1,16 @@
+﻿using AnimalFarm.Models;
+
+
+string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+
+try
+{
+    Chicken chicken = new Chicken(name, age);
+    Console.WriteLine(chicken.ToString());
+}
+catch (ArgumentException ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
