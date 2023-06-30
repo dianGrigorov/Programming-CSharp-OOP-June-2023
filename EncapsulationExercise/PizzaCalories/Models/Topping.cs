fd7bff9ffb4dc2cf9d1d07d5d1519d64f3c8d05c
dianGrigorov <diangrigorov@gmail.com>
2023-06-30 18:06:10 +0300

    .

--- EncapsulationExercise/PizzaCalories/Models/Topping.cs
+++ EncapsulationExercise/PizzaCalories/Models/Topping.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace PizzaCalories.Models;
 
-namespace PizzaCalories.Models
+public class Topping
 {
-    internal class ToppingClass
+
+    private const double BaseToppingCalories = 2;
+    private Dictionary<string, double> types;
+
+    private string type;
+    private double weight;
+
+    public Topping(string type, double weight)
     {
+        types = new Dictionary<string, double> { 
+            { "meat", 1.2}, { "veggies", 0.8},{ "cheese", 1.1},{ "sauce", 0.9} };
+
+        Type = type;
+        Weight = weight;
+    }
+
+    public string Type
+    {
+        get => type;
+        set
+        {
+            if (!types.ContainsKey(value.ToLower()))
+            {
+                throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+            }
+            type = value;
+        }
+    }
+    public double Weight
+    {
+        get => weight;
+        private set
+        {
+            if (value < 0 || value > 50)
+            {
+                throw new ArgumentException($"{Type} weight should be in the range [1..50].");
+            }
+
+            weight = value;
+        }
+    }
+
+    public double Calories
+    {
+        get
+        {
+            double typeModifier = types[Type.ToLower()];
+
+            return BaseToppingCalories * Weight * typeModifier;
+        }
     }
 }
