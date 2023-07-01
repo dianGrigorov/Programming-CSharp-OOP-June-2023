c9cbba242cc32e8d0d0185deb05dbcfbc82432f8
dianGrigorov <diangrigorov@gmail.com>
2023-07-01 14:41:27 +0300

    .

---
+++ EncapsulationExercise/FootballTeamGenerator/Models/Player.cs
@@ -0,0 +1,112 @@
+﻿namespace FootballTeamGenerator.Models;
+
+public class Player
+{
+    private const int MinStats = 0;
+    private const int MaxStats = 100;
+
+    private string name;
+
+    private int endurance;
+    private int sprint;
+    private int dribble;
+    private int passing;
+    private int shooting;
+
+    public Player(string name, int endurance, int sprint, int dribble, int passing, int shooting)
+    {
+        Name = name;
+        Endurance = endurance;
+        Sprint = sprint;
+        Dribble = dribble;
+        Passing = passing;
+        Shooting = shooting;
+    }
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentNullException("A name should not be empty.");
+            }
+            name = value;
+        }
+    }
+
+    public int Endurance
+    {
+        get => endurance;
+        set
+        {
+            if (!ValidStats(value))
+            {
+                throw new ArgumentException($"{nameof(Endurance)} should be between 0 and 100.");
+            }
+            endurance = value;
+        }
+    }
+    public int Sprint
+    {
+        get => sprint;
+        set
+        {
+            if (!ValidStats(value))
+            {
+                throw new ArgumentException($"{nameof(Sprint)} should be between 0 and 100.");
+            }
+            sprint = value;
+        }
+    }
+
+    public int Dribble
+    {
+        get => dribble;
+        set
+        {
+            if (!ValidStats(value))
+            {
+                throw new ArgumentException($"{nameof(Dribble)} should be between 0 and 100.");
+            }
+            dribble = value;
+        }
+    }
+    public int Passing
+    {
+        get => passing;
+        set
+        {
+            if (!ValidStats(value))
+            {
+                throw new ArgumentException($"{nameof(Passing)} should be between 0 and 100.");
+            }
+            passing = value;
+        }
+    }
+    public int Shooting
+    {
+        get => shooting;
+        set
+        {
+            if (!ValidStats(value))
+            {
+                throw new ArgumentException($"{nameof(Shooting)} should be between 0 and 100.");
+            }
+            shooting = value;
+        }
+    }
+
+    public double Stats
+    {
+        get => (Endurance + Sprint + Dribble + Passing + Shooting) / 5.0;
+    }
+    private bool ValidStats(int value)
+    {
+        if (value > MinStats && value < MaxStats)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ EncapsulationExercise/FootballTeamGenerator/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using FootballTeamGenerator.Models;
+
+List<Team> teams = new List<Team>();
+string input;
+
+while ((input = Console.ReadLine()) != "END")
+{
+    string[] tokens = input.Split(";", StringSplitOptions.RemoveEmptyEntries);
+    string command = tokens[0];
+
+    try
+    {
+        if (command == "Team")
+        {
+            AddTeam(tokens[1], teams);
+        }
+        else if (command == "Add")
+        {
+
+            AddPlayer(
+                    tokens[1],
+                    tokens[2],
+                    int.Parse(tokens[3]),
+                    int.Parse(tokens[4]),
+                    int.Parse(tokens[5]),
+                    int.Parse(tokens[6]),
+                    int.Parse(tokens[7])
+                    );
+        }
+        else if (command == "Remove")
+        {
+            RemovePlayer(tokens[1], tokens[2]);
+        }
+        else if (command == "Rating")
+        {
+            PrintRating(tokens[1]);
+        }
+    }
+    catch (Exception ex)
+    {
+
+        Console.WriteLine(ex.Message);
+    }
+}
+
+void AddTeam(string name, List<Team> teams)
+{
+    teams.Add(new Team(name));
+}
+
+void AddPlayer(
+    string teamName,
+    string playerName,
+    int endurance,
+    int sprint,
+    int dribble,
+    int passing,
+    int shooting
+    )
+{
+    Player player = new(playerName, endurance, sprint, dribble, passing, shooting);
+   
+    Team team = teams.FirstOrDefault(t => t.Name == teamName);
+    if (team == null)
+    {
+        throw new ArgumentException($"Team {teamName} does notexist.");
+    }
+    team.AddPlayer(player);
+}
+void RemovePlayer(string teamName, string playerName)
+{
+    Team team = teams.FirstOrDefault(t => t.Name == teamName);
+    if (team == null)
+    {
+        throw new ArgumentException($"Team {teamName} does notexist.");
+    }
+    team.RemovePlayer(playerName);
+}
+void PrintRating(string teamName)
+{
+    Team team = teams.FirstOrDefault(t => t.Name == teamName);
+    if (team == null)
+    {
+        throw new ArgumentException($"Team {teamName} does not exist.");
+    }
+    Console.WriteLine($"{teamName} - {team.Rating:f0}");
+}
