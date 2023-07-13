700617d063e3b83d6e1aeed33fa1fb5d53f2dc75
dianGrigorov <diangrigorov@gmail.com>
2023-07-13 12:10:37 +0300

    .

--- UnitTesting-Lab/Skeleton.Tests/AxeTests.cs
+++ UnitTesting-Lab/Skeleton.Tests/AxeTests.cs
@@ -1,14 +1,47 @@
 using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
     [TestFixture]
     public class AxeTests
     {
-        [Test]
-        public void Test1()
+        public void AxeDhouldInitWithCorrectValues()
         {
+            //Arrange and Act
+            Axe axe = new(100, 100);
 
+            //Assert
+            Assert.AreEqual(100, axe.DurabilityPoints);
+            Assert.AreEqual(100, axe.AttackPoints);
+        }
+        [Test]
+        public void AttackedMethodShouldDecreaseDurabilityPoints()
+        {
+            //Arrange
+            Dummy target = new(10, 10);
+            Axe axe = new(10, 10);
+
+            //Act
+            axe.Attack(target);
+
+            //Assert
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesen't change after attack.");
+        }
+        [Test]
+        public void AttackMethodShouldThorwExeptionIfDurabilityIsZero()
+        {
+            //Arrange
+            Dummy target = new(100, 100);
+            Axe axe = new(10, 10);
+
+            //Act
+            for (int i = 0; i < 10; i++)
+            {
+                axe.Attack(target);
+            }
+            //Assert
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(target), "Axe is broken.");
         }
     }
 }
--- UnitTesting-Lab/Skeleton.Tests/DummyTests.cs
+++ UnitTesting-Lab/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Runtime.InteropServices;
 
 namespace Skeleton.Tests
 {
@@ -6,8 +8,74 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void ConstructorShouldInitilizeCorrectly()
         {
+            //Arrange and Act
+            Dummy dummy = new(100, 100);
+            //Assert
+            Assert.AreEqual(100, dummy.Health);
+        }
+        [Test]
+        public void TakeAttackShouldDecreaseHelth()
+        {
+            //Arrange
+            Dummy dummy = new(100, 100);
+            //Act
+            dummy.TakeAttack(50);
+            //Assert
+            Assert.AreEqual(50, dummy.Health);
+
+        }
+        [Test]
+        public void TakeAttackShouldTrowExeptionIfDummyIsDead()
+        {
+            //Arrange
+            Dummy dummy = new(100, 100);
+            //Act
+            dummy.TakeAttack(50);
+            dummy.TakeAttack(50);
+            //Assert
+            Assert.Throws<InvalidOperationException>(
+                () => dummy.TakeAttack(50), "Dummy is dead.");
+
+        }
+        [Test]
+        public void GiveExpShouldReturnExperianceIfDummyIsDead()
+        {
+            //Arrange
+            Dummy dummy = new(100, 100);
+
+            //Act
+            dummy.TakeAttack(50);
+            dummy.TakeAttack(50);
+
+            //Assert
+            Assert.AreEqual(100, dummy.GiveExperience());
+        }
+        [Test]
+        public void GiveExpShouldThrowExceptionIfDummyIsNotDead()
+        {
+            //Arrange
+            Dummy dummy = new(100, 100);
+
+            //Act
+            dummy.TakeAttack(50);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(
+                () => dummy.GiveExperience(), "Target is not dead.");
+        }
+        [Test]
+        public void IsDeadShouldCheckIfHealthIsLowOrEqualToZero()
+        {
+            //Arrange
+            Dummy dummy = new(50, 100);
+
+            //Act
+            dummy.TakeAttack(50);
+
+            //Assert
+            Assert.IsTrue(dummy.IsDead());
 
         }
     }
